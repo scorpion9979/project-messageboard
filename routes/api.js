a083afe72552692581560b2ebcaedafe2146ac66
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2018-10-22 19:09:39 +0300

    Added basic mongoose schemas and models

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,36 @@
 'use strict';
 
 var expect = require('chai').expect;
+var mongoose = require('mongoose');
+require('dotenv').config();
+const CONNECTION_STRING = process.env.DB;
+
+mongoose.connect(CONNECTION_STRING);
+
+const Schema = mongoose.Schema;
+
+// thread:
+// board is post param
+// _id, text, created_on, bumped_on, reported(boolean), delete_password, & replies(array)
+const threadSchema = new Schema({
+  text: {type: String, required: true},
+  created_on: {type: String, required: false, default: () => new Date().toISOString()},
+  bumped_on: {type: String, required: false, default: () => new Date().toISOString()},
+  reported: {type: Boolean, required: false, default: false},
+  delete_password: {type: String, required: true},
+  replies: {type: [String], required: false, default: []},
+});
+const Thread = mongoose.model('Thread', threadSchema);
+
+// thread reply:
+// _id, text, created_on, delete_password, & reported
+const replySchema = new Schema({
+  text: {type: String, required: true},
+  created_on: {type: String, required: false, default: () => new Date().toISOString()},
+  delete_password: {type: String, required: true},
+  reported: {type: Boolean, required: false, default: false},
+});
+const Reply = mongoose.model('Reply', replySchema);
 
 module.exports = function(app) {
 
